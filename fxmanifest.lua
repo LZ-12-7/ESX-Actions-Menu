233cc1ec41b9a3157e119df4fb7e3b89032ab9d0
LZ <chingcheng949@gmail.com>
2022-03-20 16:27:46 +0100

    Archivos

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "LZ#0001"
+contributor "Reysel"
+
+client_scripts {
+    "client/menuLZ.lua",
+    "client/cl_takehostage.lua",
+    "client/cl_piggyback.lua",
+    "client/cl_carry.lua",
+}
+
+server_scripts {
+    "server/sv_takehostage.lua",
+    "server/sv_piggyback.lua",
+    "server/sv_carry.lua",
+}
